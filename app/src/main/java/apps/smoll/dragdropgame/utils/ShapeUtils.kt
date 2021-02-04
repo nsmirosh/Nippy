4768d1af7b5ba2fb531f7eb728f537f100896d33
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2021-02-04 13:17:11 +0200

    More optimizations

--- app/src/main/java/apps/smoll/dragdropgame/utils/ShapeUtils.kt
+++ app/src/main/java/apps/smoll/dragdropgame/utils/ShapeUtils.kt
@@ -5,56 +5,48 @@ import apps.smoll.dragdropgame.Shape
 import java.util.*
 
 
-fun buildShapesWithRandomColorsAndShapeTypes(level: Int, widthAndHeight: Pair<Int, Int>): List<Shape> {
+fun buildShapesWithRandomColorsAndShapeTypes(
+    level: Int,
+    widthAndHeight: Pair<Int, Int>
+): List<Shape> {
 
     val listOfCoordinates = generateCoordinatesForShapesOnScreen(level, widthAndHeight)
 
-    val shuffledShapes = getShuffledShapeArray().toMutableList()
-    val shuffledColors = getShuffledColorsArray()
+    val shuffledShapes = getShuffledShapes()
+    val shuffledColors = getShuffledColors()
 
-
-   /* return listOfCoordinates.map {
-        Shape(
-
-            it,
-            shuffledShapes
-        )
-
-    }*/
-
-    return listOfCoordinates.mapIndexed { index, coords ->
+    return listOfCoordinates.map { coords ->
         Shape(
             coords,
-            shuffledShapes[index],
-            shuffledColors[index]
+            shuffledShapes.pop(),
+            shuffledColors.pop()
         )
     }
 }
 
-private fun getShuffledShapeArray(): Array<Int> {
+private fun getShuffledShapes(): Stack<Int> {
     val imageShapeArray = arrayOf(
         R.drawable.ic_square,
         R.drawable.ic_hexagonal,
         R.drawable.ic_star,
         R.drawable.ic_circle
     )
-    imageShapeArray.shuffle()
-    return imageShapeArray
+    return Stack<Int>().apply {
+        addAll(imageShapeArray)
+        shuffle()
+    }
 }
 
 
-private fun getShuffledColorsArray(): Array<Int> {
+private fun getShuffledColors(): Stack<Int> {
     val colorsArray = arrayOf(
         R.color.color_1,
         R.color.color_2,
         R.color.color_3,
         R.color.color_4,
     )
-
-    colorsArray.shuffle()
-
-    val stack = Stack<Int>()
-    
-    stack.addAll(colorsArray)
-    return colorsArray
+    return Stack<Int>().apply {
+        addAll(colorsArray)
+        shuffle()
+    }
 }
