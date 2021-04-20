8362399e05631f38f1e0ee136dfbfc02360afdaa
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2021-04-20 19:43:51 +0300

    Added databinding in the GameFragment'

--- app/src/main/java/apps/smoll/dragdropgame/features/game/GameFragment.kt
+++ app/src/main/java/apps/smoll/dragdropgame/features/game/GameFragment.kt
@@ -6,26 +6,46 @@ import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.DragEvent
+import android.view.LayoutInflater
 import android.view.MotionEvent.ACTION_DOWN
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import apps.smoll.dragdropgame.R
 import apps.smoll.dragdropgame.Shape
+import apps.smoll.dragdropgame.databinding.FragmentGameBinding
 import apps.smoll.dragdropgame.features.menu.MenuFragmentDirections
 import apps.smoll.dragdropgame.utils.*
 import kotlinx.android.synthetic.main.fragment_game.*
 import timber.log.Timber
 
 
-class GameFragment : Fragment(R.layout.fragment_game) {
+class GameFragment : Fragment() {
 
     val gameViewModel: GameViewModel by viewModels()
 
+    lateinit var binding: FragmentGameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+        with(binding) {
+            lifecycleOwner = viewLifecycleOwner
+            viewmodel = gameViewModel
+        }
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         startObservingLiveData()
         initListeners()
         with(containerView) {
@@ -36,21 +56,6 @@ class GameFragment : Fragment(R.layout.fragment_game) {
     }
 
     private fun startObservingLiveData() {
-        gameViewModel.scoreText.observe(
-            viewLifecycleOwner,
-            { scoreTextView.text = it }
-        )
-
-        gameViewModel.timerText.observe(
-            viewLifecycleOwner,
-            { timeLeftTextView.text = it }
-        )
-
-        gameViewModel.levelText.observe(
-            viewLifecycleOwner,
-            { levelTextView.text = it }
-        )
-
         gameViewModel.shapeToMatch.observe(
             viewLifecycleOwner,
             { updateShapeToMatch(it) }
